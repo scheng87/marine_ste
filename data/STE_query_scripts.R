9059c500f47f0036ee85ab26b4b1302df3435c2a
scheng87 <cheng@nceas.ucsb.edu>
2018-05-11 15:13:17 -0700

    Updated query scripts to reflect new data structure (both synergies and tradeoffs) as well as color scheme changed to blue

--- data/STE_query_scripts.R
+++ data/STE_query_scripts.R
@@ -36,11 +36,11 @@ io_counts <- as.data.frame(io_counts)
 io_counts$Out_type <- rownames(io_counts)
 io_counts <- gather(io_counts,"Int_type","n",1:4)
 
-pdf(file="STE_Int_Outcome_Heatmap_articles_4_24.pdf", width=11, height=8.5)
+pdf(file="STE_Int_Outcome_Heatmap_articles_test.pdf", width=11, height=8.5)
 ggplot(io_counts, aes(x=Out_type,y=Int_type,fill=n)) +
-  geom_tile(color="white",size=0.1) +
+  geom_tile(color="gray90",size=0.1) +
   geom_text(aes(label=n, color="white")) +
-  scale_fill_viridis(name="# Cases",na.value="gray90") +
+  scale_fill_gradient(low="#f7fbff",high="#084594",name="# Cases",na.value="gray90") +
   coord_equal() +
   theme_tufte(base_family="Helvetica") +
   theme(axis.ticks=element_line(size=0.4)) +
@@ -75,9 +75,9 @@ all_poss <- expand.grid(Out_type=unique(matrix2$Out_type),ste_type=unique(matrix
 all_merge <- merge(matrix2,all_poss,by=c("Out_type","ste_type","Int_type"),all=TRUE) %>% arrange(Int_type)
 
 gg <- ggplot(all_merge, aes(x=ste_type,y=Out_type,fill=n)) + 
-  geom_tile(color="white",size=0.1) +
+  geom_tile(color="gray90",size=0.1) +
   geom_text(aes(label=n, color="white")) +
-  scale_fill_viridis(name="# Cases",na.value="gray90") +
+  scale_fill_gradient(low="#f7fbff",high="#084594",name="# Cases",na.value="gray90") +
   coord_equal() +
   facet_wrap(~Int_type, ncol=3) +
   labs(x="STE Type",y=NULL,title="STEs observed in domains of human well-being amongst marine conservation interventions (193 cases in 77 articles)") +
@@ -97,7 +97,7 @@ gg <- ggplot(all_merge, aes(x=ste_type,y=Out_type,fill=n)) +
   theme(legend.key.width=unit(1, "cm")) +
   theme(axis.text.x = element_text(angle=45,hjust=1,size=7))
 
-pdf(file="STE_type_Int_Outcome_Heatmap_4_24.pdf", width=11, height=8.5)
+pdf(file="STE_type_Int_Outcome_Heatmap_5_11.pdf", width=11, height=8.5)
 gg
 dev.off()
 
@@ -134,72 +134,23 @@ map <- readShapeSpatial("data/TM_WORLD_BORDERS-0.3/TM_WORLD_BORDERS-0.3.shp")
 plot(map)
 map <- fortify(map, region="ISO3")
 
-pdf(file="STE_Country_Map_dark_4_24.pdf", width=16, height=8.5)
+pdf(file="STE_Country_Map_dark_5_11.pdf", width=16, height=8.5)
 ggplot() + 
   geom_map(data=countries_only, aes(map_id=Code, fill=counts),map=map) + 
   geom_map(data=countries_zero, aes(map_id=Code),fill="#f0f0f0",map=map) + 
   expand_limits(x=map$long,y=map$lat) + 
   theme(panel.background = element_rect(fill = "darkgray", colour = "darkgray"), panel.grid.major = element_blank(), panel.grid.minor = element_blank()) + 
-  scale_fill_gradient2(low="#d9f0a3",mid="#41ab5d",high="#004529",midpoint=(max(countries_only$counts)/2),limits=c(0,max(countries_only$counts)))
+  scale_fill_gradient2(low="#f7fbff",mid="#6baed6",high="#08306b",midpoint=(max(countries_only$counts)/2),limits=c(0,max(countries_only$counts)))
 dev.off()
-
-###########################################################
-##d3 Force network - node colors indicate outcome domain, each node is an individual outcome
-
-#Filter dataset to those that only look at STE by domain
-subset <- filter(data, STE_domain != "NA") %>% distinct()
-mpa_outcomes <- filter(subset, Int_type == "MPA") %>% select(aid,oid,Int_type,Out_type,STE_domain) %>% arrange(aid)
-
-cbnrm_outcomes <- filter(subset, Int_type == "CBNRM") %>% select(aid,oid,Int_type,Out_type,STE_domain) %>% arrange(aid)
-
-cert_outcomes <- filter(subset, Int_type == "Certification") %>% select(aid,oid,Int_type,Out_type,STE_domain) %>% arrange(aid)
-
-#Create input
-aid <- as.list(unique(mpa_outcomes$aid))
-
-links <- matrix(nrow=1,ncol=3)
-colnames(links) <- c("source","target","aid")
-links <- as.data.frame(links)
-
-for(i in aid){
-  sub <- filter(mpa_outcomes, aid == i)
-  if(nrow(sub) > 1){
-    sub2 <- as.data.frame(t(combn(sub$Out_type, 2)))
-    colnames(sub2) <- c("source","target")
-    sub2$aid <- c(i)
-    links <- bind_rows(links,sub2)
-  } else 
-    links
-}
-
-links <- slice(links,-1)
-
-##Count links 
-counts <- count(links,source,target)
-colnames(counts) <- c("source","target","value") 
-counts <- as.data.frame(counts)
-counts$source=as.numeric(as.factor(counts$source))-1
-counts$target=as.numeric(as.factor(counts$target))-1
-##Define node IDs
-# nodes <- data.frame(name=out_type,group=c(1:6))
-# nodes <- as.data.frame(nodes)
-
-nodes <- select(mpa_outcomes,aid,Out_type)
-nodes <- as.data.frame(nodes)
-colnames(nodes) <- c("name","group")
-
-#Simple network
-simple <- select(links,-aid)
-d3SimpleNetwork(simple, file="SimpleNetwork.html")
-
-#Force network
-d3ForceNetwork(Links=counts, Nodes=nodes, Source="source",Target="target",Value="value",NodeID="name",Group="group",opacity=0.9,file="ForceNetwork.html")
 
 ########
 ## Trying with chord diagram instead
 library(circlize)
 library(reshape)
 
+###########################
+##FOR MPAS
+###########################
 syn <- filter(mpa_outcomes,STE_domain == "Synergy") %>% arrange(Out_type,aid)
 tradeoff <- filter(mpa_outcomes,STE_domain == "Tradeoff") %>% arrange(Out_type,aid)
 both <- filter(mpa_outcomes,STE_domain == "Both") %>% arrange(Out_type,aid)
@@ -292,7 +243,9 @@ title("Synergies (n=26 articles, x=70 cases)",line=-3.5)
 mtext("STE by outcome domain type in MPAs",outer=TRUE,cex=1.5)
 dev.off()
 
+###########################
 ##for CBNRM
+###########################
 syn <- filter(cbnrm_outcomes,STE_domain == "Synergy") %>% arrange(Out_type,aid)
 tradeoff <- filter(cbnrm_outcomes,STE_domain == "Tradeoff") %>% arrange(Out_type,aid)
 both <- filter(cbnrm_outcomes,STE_domain == "Both")
@@ -386,88 +339,6 @@ title("Both (n=8 articles, x=21 cases)",line=-3.5)
 mtext("STE by outcome domain type in CBNRM",outer=TRUE,cex=1.5)
 dev.off()
 
-##for Certification
-syn <- filter(cert_outcomes,STE_domain == "Synergy") %>% arrange(Out_type,aid)
-tradeoff <- filter(cert_outcomes,STE_domain == "Tradeoff") %>% arrange(Out_type,aid)
-pos_neu <- filter(cert_outcomes,STE_domain == "Pos/neutral")
-
-#Create input for tradeoffs
-aid <- as.list(unique(tradeoff$aid))
-
-links <- matrix(nrow=1,ncol=3)
-colnames(links) <- c("source","target","aid")
-links <- as.data.frame(links)
-
-for(i in aid){
-  sub <- filter(tradeoff, aid == i)
-  if(nrow(sub) > 1){
-    sub2 <- as.data.frame(t(combn(sub$Out_type, 2)))
-    colnames(sub2) <- c("source","target")
-    sub2$aid <- c(i)
-    links <- bind_rows(links,sub2)
-  } else 
-    links
-}
-
-links <- slice(links,-1)
-
-##Count links 
-counts <- count(links,source,target)
-colnames(counts) <- c("source","target","value") 
-counts <- as.data.frame(counts)
-
-##For synergies
-
-aid2 <- as.list(unique(syn$aid))
-
-links2 <- matrix(nrow=1,ncol=3)
-colnames(links2) <- c("source","target","aid")
-links2 <- as.data.frame(links2)
-
-for(i in aid2){
-  sub <- filter(syn, aid == i)
-  if(nrow(sub) > 1){
-    sub2 <- as.data.frame(t(combn(sub$Out_type, 2)))
-    colnames(sub2) <- c("source","target")
-    sub2$aid <- c(i)
-    links2 <- bind_rows(links2,sub2)
-  } else 
-    links2
-}
-
-links2 <- slice(links2,-1)
-
-##Count links 
-counts2 <- count(links2,source,target)
-colnames(counts2) <- c("source","target","value") 
-counts2 <- as.data.frame(counts2)
-
-grid.col <- c("Economic well-being"="#66c2a5","Social capital"="#fc8d62", "Culture"="#8da0cb", "Political empowerment"="#e78ac3","Education"="#a6d854","Health"="#ffd92f")
-pdf(file="STE by Domain - Cert_4_24.pdf",width=11,height=8.5)
-par(mfrow=c(1,2),oma=c(0,0,2,0))
-chordDiagram(as.data.frame(counts), transparency = 0.3, grid.col=grid.col)
-title("Tradeoffs (n=1 articles, x=2 cases)",line=-3.5)
-chordDiagram(as.data.frame(counts2), transparency = 0.3, grid.col=grid.col)
-title("Synergies (n=2 articles, x=4 cases)",line=-3.5)
-mtext("STE by outcome domain type in Certification",outer=TRUE,cex=1.5)
-dev.off()
-############
-#Circle packing to look at outcome subtypes within outcome domains
-
-##Create input
-outcomes <- select(data,aid,oid,Int_type,Out_type,Out_subtype)
-foo <- data.frame(do.call('rbind', strsplit(as.character(outcomes$Out_subtype),', ',fixed=TRUE)))
-
-outcomes <- bind_cols(outcomes,foo)
-outcomes <- select(outcomes,-Out_subtype)
-outcomes2 <- gather(outcomes,"X","Out_subtype",5:8)
-outcomes2 <- select(outcomes2,-X) %>% distinct()
-
-mpa_out <- filter(outcomes2,Int_type == "MPA") %>% select(oid,Out_type,Out_subtype)
-cbnrm_out <- filter(outcomes2,Int_type == "CBNRM")
-mpa_count <- count(mpa_out,Out_type,Out_subtype)
-cbnrm_count <- count(cbnrm_out,Out_type,Out_subtype)
-
 ##########
 #Treemapping to look at outcome subtypes within outcome domains
 pdf(file="Outcome_attr_by_domain_MPA.pdf")
@@ -502,3 +373,123 @@ treemap(cbnrm_count,index=c("Out_type","Out_subtype"),
         inflate.labels=F,
         title="CBNRM")
 dev.off()
+
+# ###########################################################
+# ##d3 Force network - node colors indicate outcome domain, each node is an individual outcome
+# 
+# #Filter dataset to those that only look at STE by domain
+# subset <- filter(data, STE_domain != "NA") %>% distinct()
+# mpa_outcomes <- filter(subset, Int_type == "MPA") %>% select(aid,oid,Int_type,Out_type,STE_domain) %>% arrange(aid)
+# 
+# cbnrm_outcomes <- filter(subset, Int_type == "CBNRM") %>% select(aid,oid,Int_type,Out_type,STE_domain) %>% arrange(aid)
+# 
+# cert_outcomes <- filter(subset, Int_type == "Certification") %>% select(aid,oid,Int_type,Out_type,STE_domain) %>% arrange(aid)
+# 
+# #Create input
+# aid <- as.list(unique(mpa_outcomes$aid))
+# 
+# links <- matrix(nrow=1,ncol=3)
+# colnames(links) <- c("source","target","aid")
+# links <- as.data.frame(links)
+# 
+# for(i in aid){
+#   sub <- filter(mpa_outcomes, aid == i)
+#   if(nrow(sub) > 1){
+#     sub2 <- as.data.frame(t(combn(sub$Out_type, 2)))
+#     colnames(sub2) <- c("source","target")
+#     sub2$aid <- c(i)
+#     links <- bind_rows(links,sub2)
+#   } else 
+#     links
+# }
+# 
+# links <- slice(links,-1)
+# 
+# ##Count links 
+# counts <- count(links,source,target)
+# colnames(counts) <- c("source","target","value") 
+# counts <- as.data.frame(counts)
+# counts$source=as.numeric(as.factor(counts$source))-1
+# counts$target=as.numeric(as.factor(counts$target))-1
+# ##Define node IDs
+# # nodes <- data.frame(name=out_type,group=c(1:6))
+# # nodes <- as.data.frame(nodes)
+# 
+# nodes <- select(mpa_outcomes,aid,Out_type)
+# nodes <- as.data.frame(nodes)
+# colnames(nodes) <- c("name","group")
+# 
+# #Simple network
+# simple <- select(links,-aid)
+# d3SimpleNetwork(simple, file="SimpleNetwork.html")
+# 
+# #Force network
+# d3ForceNetwork(Links=counts, Nodes=nodes, Source="source",Target="target",Value="value",NodeID="name",Group="group",opacity=0.9,file="ForceNetwork.html")
+
+###########################
+##for Certification
+###########################
+# syn <- filter(cert_outcomes,STE_domain == "Synergy") %>% arrange(Out_type,aid)
+# tradeoff <- filter(cert_outcomes,STE_domain == "Tradeoff") %>% arrange(Out_type,aid)
+# pos_neu <- filter(cert_outcomes,STE_domain == "Pos/neutral")
+# 
+# #Create input for tradeoffs
+# aid <- as.list(unique(tradeoff$aid))
+# 
+# links <- matrix(nrow=1,ncol=3)
+# colnames(links) <- c("source","target","aid")
+# links <- as.data.frame(links)
+# 
+# for(i in aid){
+#   sub <- filter(tradeoff, aid == i)
+#   if(nrow(sub) > 1){
+#     sub2 <- as.data.frame(t(combn(sub$Out_type, 2)))
+#     colnames(sub2) <- c("source","target")
+#     sub2$aid <- c(i)
+#     links <- bind_rows(links,sub2)
+#   } else 
+#     links
+# }
+# 
+# links <- slice(links,-1)
+# 
+# ##Count links 
+# counts <- count(links,source,target)
+# colnames(counts) <- c("source","target","value") 
+# counts <- as.data.frame(counts)
+# 
+# ##For synergies
+# 
+# aid2 <- as.list(unique(syn$aid))
+# 
+# links2 <- matrix(nrow=1,ncol=3)
+# colnames(links2) <- c("source","target","aid")
+# links2 <- as.data.frame(links2)
+# 
+# for(i in aid2){
+#   sub <- filter(syn, aid == i)
+#   if(nrow(sub) > 1){
+#     sub2 <- as.data.frame(t(combn(sub$Out_type, 2)))
+#     colnames(sub2) <- c("source","target")
+#     sub2$aid <- c(i)
+#     links2 <- bind_rows(links2,sub2)
+#   } else 
+#     links2
+# }
+# 
+# links2 <- slice(links2,-1)
+# 
+# ##Count links 
+# counts2 <- count(links2,source,target)
+# colnames(counts2) <- c("source","target","value") 
+# counts2 <- as.data.frame(counts2)
+# 
+# grid.col <- c("Economic well-being"="#66c2a5","Social capital"="#fc8d62", "Culture"="#8da0cb", "Political empowerment"="#e78ac3","Education"="#a6d854","Health"="#ffd92f")
+# pdf(file="STE by Domain - Cert_4_24.pdf",width=11,height=8.5)
+# par(mfrow=c(1,2),oma=c(0,0,2,0))
+# chordDiagram(as.data.frame(counts), transparency = 0.3, grid.col=grid.col)
+# title("Tradeoffs (n=1 articles, x=2 cases)",line=-3.5)
+# chordDiagram(as.data.frame(counts2), transparency = 0.3, grid.col=grid.col)
+# title("Synergies (n=2 articles, x=4 cases)",line=-3.5)
+# mtext("STE by outcome domain type in Certification",outer=TRUE,cex=1.5)
+# dev.off()
